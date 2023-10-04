a1bcfa025f6a5fffb44052b823acd1aa3792c371
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-10-04 11:35:33 +0100

    Create random greeting API

---
+++ app/controllers/greetings_controller.rb
@@ -0,0 +1,7 @@
+class GreetingsController < ApplicationController
+    def random_greeting
+      @greeting = Greeting.order("RANDOM()").first
+      render json: { message: @greeting.message }
+    end
+  end
+  
