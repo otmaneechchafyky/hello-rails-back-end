65854deabb6da3167eb8fe77755e44a38c52bd6c
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-10-04 13:26:08 +0100

    Update cors.rb to upload react app from different origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,7 @@
-# Be sure to restart your server when you modify this file.
-
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
-
-# Read more: https://github.com/cyu/rack-cors
-
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'http://192.168.8.102:3000' # Replace with the origin of your React app
+      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
+  end
+  
